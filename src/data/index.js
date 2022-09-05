d3b62c09235dbddbbe75f5bc2e120f2e5476b264
Daxsum <kaldax250@gmail.com>
2022-09-05 17:21:12 +0300

    first

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'kalab',
+    lastName: 'tenadeg',
+    email: 'kal@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'abebe',
+    lastName: 'kebede',
+    email: 'yos@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'crstiano',
+    lastName: 'ronaldo',
+    email: 'rolf@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'leonel',
+    lastName: 'messi',
+    email: 'mesi@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'devid',
+    lastName: 'bekam',
+    email: 'devid@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'aklilu',
+    lastName: 'lema',
+    email: 'aklilu@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'tsgaye',
+    lastName: 'gebre medhn',
+    email: 'gebre@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'alemayehu',
+    lastName: 'tadesse',
+    email: 'tadesse@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'valdmir',
+    lastName: 'putin',
+    email: 'putin@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'donald',
+    lastName: 'tramp',
+    email: 'tramp@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
